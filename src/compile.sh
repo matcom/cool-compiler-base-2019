e31296a9a18603056599e6e4e7aaf6423c7b96db
Alejandro Piad <alepiad@gmail.com>
2019-05-16 15:47:25 +0200

    Create compile.sh

---
+++ src/compile.sh
@@ -0,0 +1,11 @@
+# Incluya aquí las instrucciones necesarias para ejecutar su compilador
+
+INPUT_FILE=$1
+OUTPUT_FILE=$2
+
+# Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
+echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"   # Recuerde cambiar estas
+echo "Copyright 2019 Nombre1, Nombre2, Nombre3"    # líneas a los valores correctos
+
+# Llamar al compilador
+echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
