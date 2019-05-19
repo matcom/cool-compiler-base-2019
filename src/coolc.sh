d397dbf1f4010807a9561f7cef762719859abc34
Alejandro Piad <alepiad@gmail.com>
2019-05-19 09:46:32 +0200

    Update coolc.sh

--- src/coolc.sh
+++ src/coolc.sh
@@ -5,7 +5,7 @@ OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
 echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"   # Recuerde cambiar estas
-echo "Copyright 2019 Nombre1, Nombre2, Nombre3"    # líneas a los valores correctos
+echo "Copyright (c) 2019: Nombre1, Nombre2, Nombre3"    # líneas a los valores correctos
 
 # Llamar al compilador
 echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
